e4586ebe9c695cd582c5cd99d1af70769d0395c5
Анастасия Запорожец <p1ink.gg@mail.ru>
2022-10-14 20:51:40 +0300

    PracticCotlin_2

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,50 @@
+
+
+
+    fun main(args: Array<String>) {
+        //Задача 1
+        val age1 = 42
+        val age2 = 21
+        println("Задача1")
+        println(age1)
+        println(age2)
+
+        //Задача 2
+        val avg = (age1+age2)/2
+        println ("Задача2")
+        println (avg)
+
+        //Задача 3
+        age1.toDouble()
+        age2.toDouble()
+
+        //Задача 4
+        val firstName = "Dmitrii"
+        val lastName = "Zaporozhets"
+
+        // Задача 5
+        val FullName = firstName +" "+ lastName
+        println("Задача 5")
+        println (FullName)
+
+        //Задача 6
+        val MyDetails = ("Здравствуйте, меня зовут $FullName ")
+        println ("Задача 6")
+        println(MyDetails)
+
+        //Задача 7
+        val Triple: Triple< Int, Int, Int  > = Triple(10, 10, 2022)
+
+        //Задача 8
+        val day = Triple.first
+        val month = Triple.second
+        val years = Triple.third
+
+        //Задача 9
+       println("задача 9")
+        println (month)
+        println( years)
+
+        //Задача 10
+       // val Pair
+}
